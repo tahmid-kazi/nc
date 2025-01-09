9a763c4b8faadc85a8b8944fa21100f42fd40769
tahmid-kazi <101431441+tahmid-kazi@users.noreply.github.com>
2025-01-08 19:15:00 -0500

    unit 3 (lists) task 3 (list concatenation) done

---
+++ python_for_coding_interviews/3.lists/13.list_concat.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# 1/8/25 - (712 to 715pm)
+def combine_elements(arr1: List[int], arr2: List[int]) -> List[int]:
+    bruh = arr1+arr2
+    return bruh
+
+
+
+# do not modify below this line
+arr1 = [1, 3, 5]
+arr2 = [4, 6, 8]
+
+print(combine_elements(arr1, arr2))
+print(arr1)
+print(arr2)
