8cb57567d98ccbf29f2e33f2e8c938a7122c8ae4
tahmid-kazi <101431441+tahmid-kazi@users.noreply.github.com>
2025-01-14 10:49:00 -0500

    1.classes_and_object/1.what_are_objects.py

---
+++ object_oriented_python/1.classes_and_object/1.what_are_objects.py
@@ -0,0 +1,12 @@
+
+# done (1/14/25)(1045-1049am)(Tue)
+class Pet:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+
+
+# Do not modify below this line
+my_pet = Pet("Fluffy", "cat")
+print(f"My pet is a {my_pet.species} named {my_pet.name}")
